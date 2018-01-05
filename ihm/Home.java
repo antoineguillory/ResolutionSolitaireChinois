78761fdc5eb5d23cdcf321b1776fb1d206b2af8a
Antoine Guillory <antoineguillory@hotmail.fr>
2018-01-05 20:57:20 +0100

    Remove a Bug

--- ihm/Home.java
+++ ihm/Home.java
@@ -11,27 +11,31 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Home {
+	
 	private JFrame mainPanel;
+	
+	//Liste des boutons de modes
 	private ArrayList<JButton> types;
 	
 	public Home(){
+		
 		this.mainPanel = new JFrame("Resolution de solitaire chinois");
 		this.types     = new ArrayList<JButton>();
 		
 		mainPanel.setVisible(true);
 		mainPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		mainPanel.pack();
-		
+				
 		JButton type1 = new JButton("Type 1");
 		JButton type2 = new JButton("Type 2");
 		
+		//Si appui démarrage en plateau 1
 		type1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				new BoardView(1);
 			}
 		});
 		
+		//Si appui démarrage en plateau 2
 		type2.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				new BoardView(2);
@@ -41,11 +45,14 @@ public class Home {
 		types.add(type1);
 		types.add(type2);
 		
+		//Place components
 		JPanel p = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		for(JButton b : types){
 			p.add(b);
 		}
 		mainPanel.add(p);
+		mainPanel.pack();
+
 	}
 	
 	public static void main(String[] args) {
